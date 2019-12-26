e17304afdfecc770cc40ff099f0cd4f4419ee451
carlos de aguas manga <cdeaguasm@gmail.com>
2019-12-26 12:09:13 -0500

    Add project structure and hubs for real time connections

---
+++ Vote/Hubs/VoteHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Vote.Hubs
+{
+    public class VoteHub : Hub
+    {
+        public async Task SendVote(string identifier)
+        {
+            await Clients.All.SendAsync("ReceiveVote", identifier);
+        }
+    }
+}
---
+++ Vote/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/voteHub").build();
+
+connection.on("ReceiveVote", function (identifier) {
+    var element = document.getElementById(identifier);
+    var value = parseInt(element.innerText);
+    element.innerText = value + 1;
+});
+
+connection.start().then(function () {
+    console.log('Start connection');
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.querySelectorAll('.btn-demab').forEach(item => {
+    console.log(item);
+    item.addEventListener("click", function (event) {
+        connection.invoke("SendVote", item.childNodes[2].id).catch(function (err) {
+            return console.error(err.toString());
+        });
+        event.preventDefault();
+    });
+});
+
+document.querySelectorAll('.btn-danger').forEach(item => {
+    item.addEventListener("click", function (event) {
+        connection.invoke("SendVote", item.childNodes[2].id).catch(function (err) {
+            return console.error(err.toString());
+        });
+        event.preventDefault();
+    });
+});
